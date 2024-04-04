e9508b3a8685801fb6e90f9a66aec46f483da573
Hofer Lukas <csaz9699@student.uibk.ac.at>
2024-04-04 10:29:42 +0200

    Header

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/views/Login";
 import Signup from "./components/views/Signup";
@@ -8,36 +8,45 @@ import Dashboard from "./components/views/Dashboard";
 import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
 import UserForm from "./components/views/UserForm";
-import NeueBestellungen from './components/views/Bestellungen/NeueBestellungen';
-import NewGroceries from './components/views/groceries/NewGroceries';
-
-
+import NeueBestellungen from "./components/views/Bestellungen/NeueBestellungen";
+import NewGroceries from "./components/views/groceries/NewGroceries";
+import Header from "./components/Header";
 
 function App() {
+    return (
+        <BrowserRouter>
+          <Header />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={<DefaultLayout />} />
+                <Route path="/" element={<GuestLayout />} />
 
-        <Route path="/" element={<DefaultLayout />} />
-        <Route path="/" element={<GuestLayout />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route
+                    path="/users/new"
+                    element={<UserForm key="userCreate" />}
+                />
+                <Route
+                    path="/users/:id"
+                    element={<UserForm key="userUpdate" />}
+                />
+                <Route path="/neworder" element={<NeueBestellungen />} />
+                <Route path="/newgroceries" element={<NewGroceries />} />
 
-        <Route path="/users" element={<Users />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users/new" element={<UserForm key="userCreate" />} />
-        <Route path="/users/:id" element={<UserForm key="userUpdate" />} />
-        <Route path="/neworder" element={<NeueBestellungen />} />
-        <Route path="/newgroceries" element={<NewGroceries />} />
-        
-        
-        <Route path="*" element={<div><h1> ERROR 404 - NOT FOUND VIEW!!!</h1></div>} />
-
-
-      </Routes>
-    </BrowserRouter>
-  )
+                <Route
+                    path="*"
+                    element={
+                        <div>
+                            <h1> ERROR 404 - NOT FOUND VIEW!!!</h1>
+                        </div>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
