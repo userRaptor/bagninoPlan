5bc7ae640c6f893dd756bbedf6090bab60fc5d4b
Hofer Lukas <csaz9699@student.uibk.ac.at>
2024-05-08 22:08:50 +0200

    Pagination bei GetGroceries

--- react/src/components/views/groceries/GetGroceries.jsx
+++ react/src/components/views/groceries/GetGroceries.jsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { Button, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import { Box, Flex } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
+import { useState } from "react";
 import {
     Table,
     Thead,
@@ -21,6 +22,9 @@ import {
 function GetGroceries() {
     const [groceries, setGroceries] = React.useState([]);
     const [search, setSearch] = React.useState("");
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 20;
 
     const fetchGroceries = () => {
         axiosClient
@@ -62,6 +66,12 @@ function GetGroceries() {
 
     const filteredGroceries = groceries.filter(grocery => grocery.name.startsWith(search));
 
+    // Pagination
+    const pages = [];
+    for (let i = 1; i <= Math.ceil(filteredGroceries.length / itemsPerPage); i++) {
+        pages.push(i);
+    }
+
     useEffect(() => {
         fetchGroceries();
     }, []);
@@ -78,13 +88,17 @@ function GetGroceries() {
                     placeholder="Search ..."
                     style={{ width: "30%" }}
                     value={search}
-                    onChange={(e) => setSearch(e.target.value)}
+                    onChange={(e) => {
+                        setSearch(e.target.value);
+                        setCurrentPage(1); // Set page back to 1 when search changes (Pagination)
+                    }}
+                    
                 />
             </div>
 
             <TableContainer>
                 <Table variant="striped" colorScheme="teal">
-                    <TableCaption>All available products</TableCaption>
+                    <TableCaption></TableCaption>
                     <Thead>
                         <Tr>
                             <Th>Id:</Th>
@@ -95,34 +109,40 @@ function GetGroceries() {
                         </Tr>
                     </Thead>
                     <Tbody>
-                        
-                        {filteredGroceries.map((grocery) => (
-                            <Tr key={grocery.id}>
+                        {filteredGroceries
+                            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage) // Pagination
+                            .map((grocery) => (
+                              <Tr key={grocery.id}>
                                 <Td>{grocery.id}</Td>
                                 <Td>{grocery.name}</Td>
                                 <Td>{grocery.unit}</Td>
                                 <Td>{grocery.category}</Td>
                                 <Td>{grocery.supplier}</Td>
                                 <Td>
-                                    {/* isDisabled */}
-                                    <Button
-                                        colorScheme="red"
-                                        onClick={() =>
-                                            deleteGroceriesById(grocery)
-                                        }
-                                    >
-                                        Delete
-                                    </Button>
+                                  <Button colorScheme="red" onClick={() => deleteGroceriesById(grocery)}>
+                                    Delete
+                                  </Button>
                                 </Td>
-                            </Tr>
-                        ))}
+                              </Tr>
+                            ))
+                        }
                     </Tbody>
                 </Table>
             </TableContainer>
 
+            {/* Pagination */}
+            <div>
+                <Text as='b' fontSize='lg' mb={"20px"} mr={"20px"}>Pagination, Select your page:</Text>
+                {pages.map((number) => (
+                    <Button key={number} onClick={() => setCurrentPage(number)} style={{ marginRight: '10px', marginBottom: '10px' }}>
+                        {number}
+                    </Button>
+                ))}
+            </div>
+
             <div style={{ display: "flex", justifyContent: "center" }}>
                 {/* isDisabled */}
-                <Button colorScheme="red" onClick={deleteAll}>
+                <Button mt={"20px"} colorScheme="red" onClick={deleteAll}>
                     DELETE ALL
                 </Button>
             </div>
